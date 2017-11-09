6e10d6c5b4f7e5d8ac29cb238bbc0bdd161f09ec
Sebastian1705 <sebaaguirre2012gmail.com>
2017-11-09 12:23:44 -0300

    auto

--- auto semaforo reloj/main/auto.cpp
+++ auto semaforo reloj/main/auto.cpp
@@ -8,6 +8,7 @@
 #define ENB 5 
 #define IN3 7 //Derecha retrocede
 #define IN4 6 //Derecha avanza
+#define LED 13
 
 #define AUTO_AVANZAR 0
 #define AUTO_RETROCEDER 1
@@ -17,82 +18,125 @@
 #define AUTO_IZQ_REV 5
 #define AUTO_STOP 6
 
+volatile int state = LOW;
 static int state_auto;
+static int ABS = 135;
+char getstr;
 
 void auto_setup()
 {
+  pinMode(LED, OUTPUT);
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
   pinMode(ENA, OUTPUT);
   pinMode(ENB, OUTPUT);
-  digitalWrite(ENA, HIGH);
-  digitalWrite(ENB, HIGH);
   Serial.begin(9600);
   state_auto = AUTO_STOP;
-  
+  auto_stop();
 }
+
 void auto_loop()
 {
   
-  auto_accion();
+  
+    getstr = Serial.read();
+
+    switch(getstr)
+    {
+      case 'f':
+        auto_avanzar();
+        break;
+      case 'b':
+        auto_retroceder();
+        break;
+      case 'l':
+        auto_izquierda();
+        break;
+      case 'r':
+        auto_derecha();
+        break;
+      case 's':
+        auto_stop();
+        break;
+      case 'A':
+        void stateChange();
+        break;  
+    }
+
 }
 
-void auto_accion()
+void auto_avanzar()
 {
-  if (timer_waitMs(RELOJ_TIMER_5, 1000))
-  {
-    switch (state_auto)
-    {
-      case AUTO_AVANZAR:
-        digitalWrite(IN1, LOW);
-        digitalWrite(IN2, HIGH);        
-        digitalWrite(IN3, LOW);         
-        digitalWrite(IN4, HIGH);        
-        Serial.println("AVANZAR");
-        break;
-      case AUTO_RETROCEDER:
-        digitalWrite(IN1, HIGH);
-        digitalWrite(IN2, LOW);       
-        digitalWrite(IN3, HIGH);      
-        digitalWrite(IN4, LOW);       
-        Serial.println("RETROCEDER");
-        break;
-      case AUTO_DERECHA:
-        digitalWrite(IN3, LOW);
-        digitalWrite(IN4, HIGH);
-        digitalWrite(IN1, LOW);
-        digitalWrite(IN2, LOW);
-        Serial.println("DERECHA");
-        break;
-      case AUTO_IZQUIERDA:
-        digitalWrite(IN1, LOW);
-        digitalWrite(IN2, HIGH);
-        digitalWrite(IN3, LOW);
-        digitalWrite(IN4, LOW);
-        Serial.println("IZQUIERDA");
-        break;
-      case AUTO_DER_REV:
-        digitalWrite(IN3, HIGH);
-        digitalWrite(IN4, LOW);
-        digitalWrite(IN1, LOW);
-        digitalWrite(IN2, LOW);
-        Serial.println("DERECHA REVERSA");
-        break;
-      case AUTO_IZQ_REV:
-        digitalWrite(IN1, HIGH);
-        digitalWrite(IN2, LOW);
-        digitalWrite(IN3, LOW);
-        digitalWrite(IN4, LOW);
-        Serial.println("IZQUIERDA REVERSA");
-        break;
-      case AUTO_STOP:
-        digitalWrite(IN1, LOW);
-        digitalWrite(IN2, LOW);
-        digitalWrite(IN3, LOW);
-        digitalWrite(IN4, LOW);
-        break;
-    }
-  }
+  digitalWrite(ENA, HIGH);
+  digitalWrite(ENB, HIGH);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);        
+  digitalWrite(IN3, LOW);         
+  digitalWrite(IN4, HIGH);        
+  Serial.println("AVANZAR");
 }
+void auto_retroceder()
+{
+  digitalWrite(ENA, HIGH);
+  digitalWrite(ENB, HIGH);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);       
+  digitalWrite(IN3, HIGH);      
+  digitalWrite(IN4, LOW);       
+  Serial.println("RETROCEDER");
+}
+void auto_derecha()
+{
+  analogWrite(ENA,ABS);
+  analogWrite(ENB,ABS);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  Serial.println("DERECHA");
+}
+void auto_izquierda()
+{
+  analogWrite(ENA,ABS);
+  analogWrite(ENB,ABS);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  Serial.println("IZQUIERDA");
+}
+void auto_derechaRev()
+{
+  analogWrite(ENA,ABS);
+  analogWrite(ENB,ABS);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  Serial.println("DERECHA REVERSA");
+}
+void auto_izquierdaRev()
+{
+  analogWrite(ENA,ABS);
+  analogWrite(ENB,ABS);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  Serial.println("IZQUIERDA REVERSA");
+}    
+void auto_stop()
+{
+  digitalWrite(ENA, LOW);
+  digitalWrite(ENB, LOW);
+  Serial.println("STOP");
+}    
+
+void stateChange()
+{
+  state = !state;
+  digitalWrite(LED, state);  
+}
+       
--- auto semaforo reloj/main/auto.h
+++ auto semaforo reloj/main/auto.h
@@ -1,3 +1,11 @@
 void auto_setup(void);
 void auto_loop(void);
-void auto_accion(void);
+void auto_avanzar();
+void auto_retroceder();
+void auto_derecha();
+void auto_izquierda();
+void auto_derechaRev();
+void auto_izquierdaRev();
+void auto_stop();
+
+void stateChange();
